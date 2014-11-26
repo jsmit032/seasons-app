547108fadc0f06d70e08f6608098fc20776fd4e3
stephjin <stephanie.k.jin@gmail.com>
2014-11-25 17:38:59 -0800

    set initial forecast by geolocation.

--- app/assets/javascripts/angular/shared/factory.js
+++ app/assets/javascripts/angular/shared/factory.js
@@ -1,17 +1,15 @@
 angular.module('SeasonsApp')
 
-
-// // factory returns object with keys that can be used in the controller to call function
-// // $http calls 
+// factory returns object with keys that can be used in the controller to call function
 .factory('weatherService', ['$http', '$q', function($http, $q) {
   function getWeather(zip) {
     var deferred = $q.defer();
-    $http.get('http://www.corsproxy.com/api.wunderground.com/api/2f0b44146ceab5a4/hourly/q/' + zip + '.json')
+    $http.get('http://www.corsproxy.com/api.wunderground.com/api/2f0b44146ceab5a4/forecast/conditions/q/' + zip + '.json')
       .success(function(data) {
         deferred.resolve(data);
       })
       .error(function(err) {
-        console.log('Error retrieving markets');
+        console.log('Error retrieving data');
         deferred.reject(err);
       });
     return deferred.promise;
@@ -20,26 +18,3 @@ angular.module('SeasonsApp')
     getWeather: getWeather
   };
 }]);
-
-
-
-
-// yahoo weather api
-// .factory('weatherService', ['$http', '$q', function ($http, $q){
-//   function getWeather (zip) {
-//     var deferred = $q.defer();
-//     $http.get('https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20weather.forecast%20WHERE%20location%3D%22' + zip + '%22&format=json&diagnostics=true&callback=')
-//       .success(function(data){
-//         deferred.resolve(data.query.results.channel);
-//       })
-//       .error(function(err){
-//         console.log('Error retrieving markets');
-//         deferred.reject(err);
-//       });
-//     return deferred.promise;
-//   }
-  
-//   return {
-//     getWeather: getWeather
-//   };
-// }]);
